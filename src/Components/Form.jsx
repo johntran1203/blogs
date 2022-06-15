f34e5c5fb5fbc13ed575a46f25c76924ce6581d0
johntran <tranjohn1211@gmail.com>
2022-06-15 16:09:03 -0700

    update form and add state to each input form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,38 @@
-const Form = () => {
+import { useState } from "react";
+
+const Form = (props) => {
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
   return (
     <form>
       <label htmlFor="author">Author:</label>
-      <input id='author' type='text'></input>
+      <input
+        id="author"
+        type="text"
+        required
+        autoComplete="off"
+        autoFocus
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      ></input>
       <label htmlFor="title">Title:</label>
-      <input id='title' type='text'></input>
+      <input
+        id="title"
+        type="text"
+        required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      ></input>
       <label htmlFor="body">Body:</label>
-      <input type="text" name="body" id="body" />
+      <input
+        type="text"
+        required
+        name="body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      ></input>
       <button type="submit">Create</button>
     </form>
   );
