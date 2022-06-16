ac301e0b120918037e5e9c18c2168e76860e8210
johntran <tranjohn1211@gmail.com>
2022-06-15 17:49:24 -0700

    add delete button

--- src/Components/BlogList.jsx
+++ src/Components/BlogList.jsx
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { baseURL, config } from '../services';
+//what do we need for our request (two variables)
 
 const BlogList = ( props) => {
   // i remove the blogs where the props should be i will go back to it one day
 
   const { author, body, title} = props.blog.fields
+
+  // some async function to handle click
+  const deleteBlog = async () => {
+      // make a specific url by combining the baseURL with the blog's id
+    const blogURL = `${baseURL}/${props.blog.id}`
+  // make a delete request ot that endpointm with the config
+    await axios.delete(blogURL, config)
+
+  // trigger our useEffect
+    props.setToggleFetch((curr) => !curr)
+
+  }
+
   return (
     <div className="blog-list">
       {/* {blogs.map(blog => (
@@ -17,6 +33,7 @@ const BlogList = ( props) => {
       <h2>{author}</h2>
       <h3>{title}</h3>
       <h5>{body}</h5>
+      <button onClick={deleteBlog}>DELETE</button>
     </div>
   );
 }
